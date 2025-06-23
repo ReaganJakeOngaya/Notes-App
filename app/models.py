93f722a43cf5143ee763307f112efc24916f4186
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-06-23 11:58:20 +0300

    field length increase

--- app/models.py
+++ app/models.py
@@ -11,8 +11,8 @@ from . import db
 # ==========================
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     avatar = db.Column(db.String(500))  # URL to profile image
     bio = db.Column(db.Text)
