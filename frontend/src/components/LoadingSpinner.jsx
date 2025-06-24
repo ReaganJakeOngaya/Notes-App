8362f7140f320de098cf86047cb15659f4ab9970
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-06-25 00:06:38 +0300

    landing page

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import '../css/LoadingSpinner.css';
 
 const LoadingSpinner = ({ size = 'medium', color = 'primary', className = '' }) => {
@@ -21,12 +20,6 @@ const LoadingSpinner = ({ size = 'medium', color = 'primary', className = '' }) 
       <div className={`spinner ${sizeClasses[size]} ${colorClasses[color]}`}></div>
     </div>
   );
-};
-
-LoadingSpinner.propTypes = {
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  color: PropTypes.oneOf(['primary', 'secondary', 'light', 'dark']),
-  className: PropTypes.string
 };
 
 export default LoadingSpinner;
