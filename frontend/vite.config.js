c1d332fbb5def5cb9e6406fe1a8290ea241027ff
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-07-01 19:38:58 +0300

    removing tailwind css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-// export default defineConfig({
-//   define: {
-//     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
-//   },
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://notes-app-20no.onrender.com' || 'http://localhost:5000',
-//         changeOrigin: true,
-//         secure: false,
-//         ws: true
-//       },
-//       '/auth': {
-//         target: 'https://notes-app-20no.onrender.com' || 'http://localhost:5000',
-//         changeOrigin: true,
-//         secure: false
-//       }
-//     }
-//   }
-// });
+export default defineConfig({
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://notes-app-20no.onrender.com' || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      },
+      '/auth': {
+        target: 'https://notes-app-20no.onrender.com' || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
 
 
 
