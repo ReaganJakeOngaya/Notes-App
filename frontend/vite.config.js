a535c1adff32597941a9c5f0eac5b90f69a9ffaa
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-07-01 19:35:32 +0300

    removing tailwind css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,26 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
+// import { defineConfig } from 'vite';
 
-export default defineConfig({
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_BASE_URL || 'https://notes-app-20no.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace(/^\/api/, ''),
-        ws: true
-      },
-      '/auth': {
-        target: process.env.VITE_API_BASE_URL || 'https://notes-app-20no.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace(/^\/auth/, '')
-      }
-    }
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ]
-    }
-  }
-});
+// export default defineConfig({
+//   define: {
+//     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://notes-app-20no.onrender.com' || 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false,
+//         ws: true
+//       },
+//       '/auth': {
+//         target: 'https://notes-app-20no.onrender.com' || 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// });
+
+
+
 
