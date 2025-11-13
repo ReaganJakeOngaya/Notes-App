5a4008751140bd5915d9b0df323de68067824c32
ReaganJakeOngaya <reaganjakeongaya@gmail.com>
2025-11-13 15:07:40 +0300

    postcss configuration v2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      "!./node_modules/@fortawesome/**/*",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: {
-            DEFAULT: '#6d5dfc',
-            hover: '#5b4bdf',
-          },
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6d5dfc',
+          hover: '#5b4bdf',
         },
+
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        mono: ['Fira Code', 'monospace'],
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
